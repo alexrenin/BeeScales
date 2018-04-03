acec272d1edbfc32d75742a4311f39de98511b8c
Alexandr Belonogov <sanya_renin@mail.ru>
2018-04-03 08:51:22 +0500

    alarm work

--- SleepMode/SleepMode.ino
+++ SleepMode/SleepMode.ino
@@ -31,19 +31,8 @@ SIGNAL(TIMER0_COMPA_vect) { //прерывание считывающие мощ
 void setTime(byte sYear, byte sMonth, byte sDate,
             byte sDoW, byte sHour, byte sMinute,
             byte sSecond) {
-  Serial.print("Set time: "); 
-  Serial.print("20");
-  Serial.print(sYear);
-  Serial.print(".");
-  Serial.print(sMonth);
-  Serial.print(".");
-  Serial.print(sDate);
-  Serial.print(" ");
-  Serial.print(sHour);
-  Serial.print(":");
-  Serial.println(sMinute);
-  
-//  Clock.setClockMode(h12);  // set to 24h
+ 
+  Clock.setClockMode(h12);  // set to 24h
  
   Clock.setYear(sYear);
   Clock.setMonth(sMonth);
@@ -64,70 +53,74 @@ void getTime() {
 }
 
 void DrawMenu () {
-  getTime();
-  Serial.print("20");
-  Serial.print(year);
-  Serial.print(".");
-  Serial.print(month);
-  Serial.print(".");
-  Serial.print(date);
-  Serial.print(" ");
-  Serial.print(hour);
-  Serial.print(":");
-  Serial.print(minute);
-  Serial.print(":");
-  Serial.println(second);
-
-  // Display Alarm 1 information
-  byte ADay, AHour, AMinute, ASecond, ABits;
-  bool ADy, A12h, Apm;
-  
-  Serial.print("Alarm 1: ");
-  Clock.getA1Time(ADay, AHour, AMinute, ASecond, ABits, ADy, A12h, Apm);
-  Serial.print(ADay, DEC);
-  if (ADy) {
-    Serial.print(" DoW");
-  } else {
-    Serial.print(" Date");
-  }
-  Serial.print(' ');
-  Serial.print(AHour, DEC);
-  Serial.print(' ');
-  Serial.print(AMinute, DEC);
-  Serial.print(' ');
-  Serial.print(ASecond, DEC);
-  Serial.print(' ');
-  if (A12h) {
-    if (Apm) {
-      Serial.print('pm ');
-    } else {
-      Serial.print('am ');
-    }
-  }
-  if (Clock.checkAlarmEnabled(1)) {
-    Serial.print("enabled");
-  }
-  Serial.print('\n');
-    
+//  getTime();
+//  Serial.print("20");
+//  Serial.print(year);
+//  Serial.print(".");
+//  Serial.print(month);
+//  Serial.print(".");
+//  Serial.print(date);
+//  Serial.print(" ");
+//  Serial.print(hour);
+//  Serial.print(":");
+//  Serial.print(minute);
+//  Serial.print(":");
+//  Serial.println(second);
+//
+//  // Display Alarm 1 information
+//  byte ADay, AHour, AMinute, ASecond, ABits;
+//  bool ADy, A12h, Apm;
+//  
+//  Serial.print("Alarm 1: ");
+//  Clock.getA1Time(ADay, AHour, AMinute, ASecond, ABits, ADy, A12h, Apm);
+//  Serial.print(ADay, DEC);
+//  if (ADy) {
+//    Serial.print(" DoW");
+//  } else {
+//    Serial.print(" Date");
+//  }
+//  Serial.print(' ');
+//  Serial.print(AHour, DEC);
+//  Serial.print(' ');
+//  Serial.print(AMinute, DEC);
+//  Serial.print(' ');
+//  Serial.print(ASecond, DEC);
+//  Serial.print(' ');
+//  if (A12h) {
+//    if (Apm) {
+//      Serial.print('pm ');
+//    } else {
+//      Serial.print('am ');
+//    }
+//  }
+//  if (Clock.checkAlarmEnabled(1)) {
+//    Serial.print("enabled");
+//  }
+//  Serial.print('\n');
+//  if (Clock.checkIfAlarm(1) ) 
+//  {
+//    Serial.println("Alarm 1 Triggered");
+//  }
 }
 
 void setSleepTimer(int mins){
-//  uint8_t hours = mins / 60;
-//  uint8_t mins_left = mins - hours * 60;
-//  dt = Clock.getDateTime();
-//  uint8_t hnow = dt.hour;
-//  uint8_t mnow = dt.minute;
-//  uint8_t sec = dt.second;
-//  mnow += mins_left;
-//  if (mnow >= 60) {
-//    mnow -= 60;
-//    hnow += 1;
-//  }
-//  hnow += hours;
-//  if (hnow > 23) {
-//    hnow -= 24;
-//  }
-//  clock.setAlarm1(0, hnow, mnow, sec, DS3231_MATCH_H_M_S);
+  getTime();
+  byte A1Day = date;
+  byte A1Hour = hour;
+  byte A1Minute = minute;
+  byte A1Second = second; 
+  byte AlarmBits = 0x0; //Match Type Values. 0=Day/Date, 8=Hrs/Min/Sec, 12=Min/Sec, 14=Sec, 15=Every Sec
+  bool A1Dy = false; //date or day of week
+
+  A1Minute = A1Minute + mins;
+
+  if (A1Minute >= 60) {
+    A1Minute = A1Minute - 60;
+    A1Hour = A1Hour + 1;
+  }
+
+  Clock.setA1Time(A1Day, A1Hour, A1Minute, A1Second, AlarmBits, A1Dy, h12, PM);
+  Clock.turnOnAlarm(1);
 }
 
 void KeyPad () {
@@ -143,7 +136,17 @@ void ReadAnalog () {
    
 }
 
+void weakUP() {
+    bool g2 = digitalRead(WakePin);
+    Serial.print("");
+    Serial.print("Pin2: ");
+    Serial.println(g2); 
+    Serial.print(" ");
+}
+
 void setup() {
+  attachInterrupt(0, weakUP, CHANGE); //внешнее прерывание от RTC
+  
   OCR0A = 0xAF; //прерывание
   TIMSK0 |= _BV(OCIE0A); //прерывание
 
@@ -151,16 +154,10 @@ void setup() {
   Wire.begin();
 
   pinMode(WakePin, INPUT);
-  Clock.turnOnAlarm(1);
+  digitalWrite(WakePin, HIGH);
+//  Clock.turnOnAlarm(1);
 
-  byte A1Day = 4;
-  byte A1Hour = 20;
-  byte A1Minute = 30;
-  byte A1Second = 0; 
-  byte AlarmBits = 0;
-  bool A1Dy = false;
-  
-  Clock.setA1Time(A1Day, A1Hour, A1Minute, A1Second, AlarmBits, A1Dy, h12, PM);
+  setSleepTimer(1);
 }
 
 void loop() {
